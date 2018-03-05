8c0a5064a76815000f4ade511783b6e193f0cdf0
Jason Biegel <jason@biesel.co>
2018-03-04 23:23:50 -0500

    bump version

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import find_packages, setup
 
 setup(
     name='plant-recipe-schema',
-    version='0.2.0',
-    author='Jason Biegel, Alan Sarkisian',
+    version='0.3.0',
+    author='Jason Biegel',
     license='LICENSE',
     description='Store plant grow recipes in a structured XML format',
     packages=find_packages(exclude=['docs', 'samples', 'tests']),
