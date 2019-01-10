cc318e2c3b46aef437d1b7dca8a54d9044b6452f
Jason Biegel <jason@biesel.co>
2019-01-10 01:19:18 -0500

    include XSD file

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from setuptools import find_packages, setup
 
 setup(
     name='grow-recipe',
-    version='0.6.0',
+    version='0.8.0',
     author='Jason Biegel',
     license='LICENSE',
     description='Store plant grow recipes in a structured XML format',
     packages=find_packages(exclude=['docs', 'samples', 'tests']),
+    package_data={'grow_recipe': ['grow-recipe.xsd']},
     install_requires=[
         'lxml==4.1.1',
     ]
